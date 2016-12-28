c8066a7c0d551c40f08cd150e9b609b7060ee12d
tom13878 <tomas.morley@wur.nl>
2016-12-28 03:57:49 +0100

    added fert prices to the panel file

--- Code/panel_MWI.R
+++ Code/panel_MWI.R
@@ -6,9 +6,6 @@
 #
 # IHS3 Third Integrated Household Survey 2010-2011
 # IHPS Integrated Household Panel Survey 2013
-#
-# We need to make the household identifier
-# see the code below 
 # -------------------------------------
 
 # load packages
@@ -46,12 +43,19 @@ source(file.path(filePath, "household_MWI_2013.r"))
 source(file.path(filePath, "location_MWI_2010_11.r"))
 source(file.path(filePath, "location_MWI_2013.r"))
 
+# fertilizer price variables
+source(file.path(filePath, "fert_prices_MWI_2010_11.r"))
+source(file.path(filePath, "fert_prices_MWI_2013.r"))
+
+
 # -------------------------------------
-# join in each year first
+# join in each year first to create
+# cross sections
 # -------------------------------------
 
 # wave 1
 cross_section10_11 <- left_join(unique(location10_11), unique(HH10_11))
+cross_section10_11 <- left_join(cross_section10_11, unique(fert_prices1011))
 cross_section10_11 <- left_join(cross_section10_11, unique(oput_2010_11))
 cross_section10_11 <- left_join(cross_section10_11, unique(plotRS_2010_11))
 cross_section10_11 <- left_join(cross_section10_11, unique(areas10_11))
@@ -60,6 +64,7 @@ cross_section10_11$surveyyear <- 2011
 
 # wave 2
 cross_section13 <- left_join(unique(location13), unique(HH13))
+cross_section13 <- left_join(cross_section13, unique(fert_prices13))
 cross_section13 <- left_join(cross_section13, unique(oput2013))
 cross_section13 <- left_join(cross_section13, unique(plotRS_2013))
 cross_section13 <- left_join(cross_section13, unique(areas13))
@@ -72,16 +77,14 @@ cross_section13$surveyyear <- 2013
 
 keep <- intersect(names(cross_section10_11), names(cross_section13))
 panel <- rbind(cross_section10_11[, keep], cross_section13[, keep])
-# Note that the plot numbers are different. But not panel anyway
-# panel$plotnum <- NULL
 panel <- select(panel, HHID, case_id, ea_id, surveyyear, plotnum, everything())
 
-# select down on maize plots
+# select maize plots
 panel <- filter(panel, crop_code %in% c(1, 2, 3, 4))
 
 # take out the trash
 rm(areas10_11, areas13, cross_section10_11, cross_section13,
-dataPath, filePath, HH10_11, HH13, keep, lab2010_11, lab2013,
-location10_11, location13, oput_2010_11, oput2013,
-plot_2010_11, plot_2013, plotDS_2010_11, plotDS_2013,
-plotRS_2010_11, plotRS_2013)
+   dataPath, filePath, HH10_11, HH13, keep, lab2010_11, lab2013,
+   location10_11, location13, oput_2010_11, oput2013,
+   plot_2010_11, plot_2013, plotDS_2010_11, plotDS_2013,
+   plotRS_2010_11, plotRS_2013, fert_prices1011, fert_prices13)
