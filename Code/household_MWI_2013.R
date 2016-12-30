dd7a1b7826d285540c6b66b7645f1993ab57cc13
tom13878 <tomas.morley@wur.nl>
2016-12-30 22:31:21 +0100

    changed name of output dataframe to be consistent with other files

--- Code/household_MWI_2013.R
+++ Code/household_MWI_2013.R
@@ -15,22 +15,22 @@ if(Sys.info()["user"] == "Tomas"){
 }
 
 
-HH13 <- read_dta(file.path(dataPath, "Household/HH_MOD_B.dta")) %>%
+household_2013 <- read_dta(file.path(dataPath, "Household/HH_MOD_B.dta")) %>%
   select(y2_hhid, indidy2 = PID, status=hh_b04, sex=hh_b03,
          yob=hh_b06b, age=hh_b05a, hh_b11, years=hh_b12)
 
-HH13$years <- as.numeric(HH13$years)
-HH13$years <- ifelse(HH13$hh_b11 %in% 1, HH13$age, HH13$years)
-HH13$status <- as_factor(HH13$status)
-HH13$sex <- toupper(as_factor(HH13$sex))
-HH13$yob <- as.integer(HH13$yob)
-HH13$hh_b11 <- NULL
+household_2013$years <- as.numeric(household_2013$years)
+household_2013$years <- ifelse(household_2013$hh_b11 %in% 1, household_2013$age, household_2013$years)
+household_2013$status <- as_factor(household_2013$status)
+household_2013$sex <- toupper(as_factor(household_2013$sex))
+household_2013$yob <- as.integer(household_2013$yob)
+household_2013$hh_b11 <- NULL
 
 # make a new variable cage (cut age = cage) which splits
 # individuals according to their age group with
 # breaks at 15, 55 and the max age
 
-HH13$cage <- cut(HH13$age, breaks = c(0, 15, 55, max(HH13$age, na.rm=TRUE)),
+household_2013$cage <- cut(household_2013$age, breaks = c(0, 15, 55, max(household_2013$age, na.rm=TRUE)),
                     labels=c("0-15", "16-55", "56+"), include.lowest = TRUE, right = TRUE)
 
 # education of household members and sum
@@ -43,20 +43,20 @@ education <- read_dta(file.path(dataPath, "Household/HH_MOD_C.dta")) %>%
 
 education$education_any <- as_factor(education$education_any) # ever went to school
 
-# join with HH13 dataframe
-HH13 <- left_join(HH13, education)
-HH13 <- select(HH13, -yob)
+# join with household_2013 dataframe
+household_2013 <- left_join(household_2013, education)
+household_2013 <- select(household_2013, -yob)
 
 # summarise the data: get sum of education
 # of household members 15-55 and number of
 # household members 15:55
 
-HH13_x <- group_by(HH13, y2_hhid) %>%
+household_2013_x <- group_by(household_2013, y2_hhid) %>%
   summarise(N1555=sum(cage %in% "16-55"))
-HH13 <- left_join(HH13, HH13_x); rm(HH13_x)
+household_2013 <- left_join(household_2013, household_2013_x); rm(household_2013_x)
 
 # select only the head of the household
-HH13 <- filter(HH13, status == "HEAD") %>% select(-status, -cage)
+household_2013 <- filter(household_2013, status == "HEAD") %>% select(-status, -cage)
 
 # -------------------------------------
 # death in the family No section W in 
